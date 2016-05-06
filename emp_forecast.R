7d06b0136314e172c2bc443038f6d66037993080
brndngrhm <brendanvgraham@gmail.com>
2016-05-06 11:38:11 -0400

    auto.arima and sarima models

--- emp_forecast.R
+++ emp_forecast.R
@@ -18,47 +18,94 @@ library(forecast)
 library(fGarch)
 
 #load data in ----
+#data from http://www.adpemploymentreport.com/
+#and is # of employees in U.S. nonfarm private sector employment
+
+#http://www.bls.gov/news.release/empsit.nr0.htm
+#http://www.bls.gov/webapps/legacy/cesbtab1.htm
 x.emp <- getURL("https://raw.githubusercontent.com/brndngrhm/employment/master/emp.csv")
 emp <- as.data.frame(read.csv(text = x.emp, strip.white = T))
-emp <- na.omit(emp)
-emp <- melt(emp, id.vars = "Year")
+emp$X <- NULL
+names(emp)[2] <- 1
+names(emp)[3] <- 2
+names(emp)[4] <- 3
+names(emp)[5] <- 4
+names(emp)[6] <- 5
+names(emp)[7] <- 6
+names(emp)[8] <- 7
+names(emp)[9] <- 8
+names(emp)[10] <- 9
+names(emp)[11] <- 10
+names(emp)[12] <- 11
+names(emp)[13] <- 12
 names(emp) <- tolower(names(emp))
-names(emp)[2] <- "month"
-names(emp)[3] <- "jobs"
-emp$month2[emp$month == "Jan"] <- 1
-emp$month2[emp$month == "Feb"] <- 2
-emp$month2[emp$month == "Mar"] <- 3
-emp$month2[emp$month == "Apr"] <- 4
-emp$month2[emp$month == "May"] <- 5
-emp$month2[emp$month == "Jun"] <- 6
-emp$month2[emp$month == "Jul"] <- 7
-emp$month2[emp$month == "Aug"] <- 8
-emp$month2[emp$month == "Sep"] <- 9
-emp$month2[emp$month == "Oct"] <- 10
-emp$month2[emp$month == "Nov"] <- 11
-emp$month2[emp$month == "Dec"] <- 12
-emp <- emp %>% arrange(year, month2)
+emp2 <- melt.data.frame(emp, id.vars = "year")
+emp2 <- na.omit(emp2)
+names(emp2)[2] <- "month"
+names(emp2)[3] <- "jobs"
+emp2$month <- as.numeric(emp2$month)
+emp2$month2 <- emp2$month
+emp2$month2[emp2$month < 10] <- paste("0", emp2$month, sep="")
+emp2$date <- paste( emp2$year,emp2$month2, "01", sep="-" )
+emp2$date2 <- ymd(emp2$date)
+emp <- emp2
+emp$year <- NULL
+emp$month <- NULL
 emp$month2 <- NULL
+emp$date <- NULL
+names(emp)[2] <- "date"
+emp$year <- year(emp$date)
+emp$month <- month(emp$date, label = T)
+emp <- emp %>% select(date, year, month, jobs)
+emp <- emp %>% arrange(date)
 
-jobs <- ts(emp[3], frequency = 12)
+jobs <- ts(emp$jobs, frequency = 12)
+jobs <- jobs[1:195]
+jobs<- ts(jobs, frequency = 12)
+change <- diff(jobs)
+date2 <- emp$date[1:194]
 
-#plot ----
+#plot2 ----
 plot(jobs, type="l")
-plot(log(jobs), type="l")
+(jobs.plot <- ggplot(emp, aes(x=date, y=jobs)) + geom_line())
 acf2(jobs)
 
-diff.jobs <- diff(log(jobs))
+plot(date2, change, type="l")
+acf2(change)
+
+diff.jobs <- diff(change)
 plot(diff.jobs, type="l")
 acf2(diff.jobs)
 
-dl12.jobs <- diff(diff.jobs,12)
-plot(dl12.jobs, type="l")
-acf2(dl12.jobs, max.lag = 80)
-
 #auto-arima ----
-model1 <- auto.arima(log(jobs))
-arima.fit <- ts(fitted(arima(log(jobs), order=c(1, 2, 2), seasonal = c(0, 1, 1))), frequency = 12)
-auto.arima.forecast <- sarima.for(log(jobs),6 ,1,2,2,0,1,1, 12) #6 month forecast
+(model1 <- auto.arima(change))
+arima.fit <- ts(fitted(arima(change, order=c(0,1,1), seasonal = c(2,0,0)), frequency = 12))
+auto.arima.forecast <- sarima.for(change,6,1,0,0,0,0,0,12) #6 month forecast
+auto.arima.forecast$pred  
+
+#sarima
+uplim=4
+aicmat <- matrix(double((uplim+1)^2),uplim+1,uplim+1)
+for (i in 0:uplim){
+  for (j in 0:uplim){
+    aicmat[i+1,j+1]=sarima(change,0,1,0,i,1,j,12,details=F,tol=0.001)$AIC}}
+
+print(aicmat)
+
+aicmat2 <- matrix(double((uplim+1)^2),uplim+1,uplim+1)
+for (i in 0:uplim){
+  for (j in 0:uplim){
+    aicmat2[i+1,j+1]<-sarima(change, i, 1, j, 3, 1, 1, 12, details=F, tol=0.001)$AIC}}
+
+print(aicmat2)
+
+(sarima(change,0,1,1,4,1,1,12,details = F))
+
+(sarima <- sarima.for(change,6,0,1,1,4,1,1,12))
+
+#sarima fit
+sarima.fit <- ts(fitted(arima(change, order=c(0, 1, 1), seasonal = c(4, 1, 1))), frequency = 12)
+
 
 #regresion with arma errors ----
 jobs2 <- log(emp$jobs)
@@ -109,14 +156,21 @@ reg.arma.fit <- fitted(fit)
 #plotting stuff ----
 
 #setting plot parameters
-n <- length(jobs)
+n <- length(change)
 time <- seq(1:n)
 auto.arima.time <- seq(n+1, n+6)
+sarima.time <- seq(n+1, n+6)
 auto.arima.pred <- auto.arima.forecast$pred
+sarima.pred <- sarima$pred
 
 #base plot
-plot(time, log(jobs), type = "p", xlim=c(n-60, n+10))
+plot(time, change, type = "p", xlim=c(n-n, n+10))
+
+#adding auto-arima fit
 lines(time, arima.fit, col="red", type ="l")
+
+#adding sarima fit
+lines(time, sarima.fit, col="blue", type ="l")
 
 #adding auto.arima forecast points and lines
 points(n+1, auto.arima.forecast$pred[1])
@@ -125,10 +179,16 @@ points(n+3, auto.arima.forecast$pred[3])
 points(n+4, auto.arima.forecast$pred[4])
 points(n+5, auto.arima.forecast$pred[5])
 points(n+6, auto.arima.forecast$pred[6])
-lines(auto.arima.time, auto.arima.pred, type="l", col = "blue")
+lines(auto.arima.time, auto.arima.pred, type="l", col = "red")
 
-#actual forecast
-forecast <- data.frame(c(exp(auto.arima.forecast$pred[1]), exp(auto.arima.forecast$pred[2]),
-                         exp(auto.arima.forecast$pred[3]), exp(auto.arima.forecast$pred[4]),
-                         exp(auto.arima.forecast$pred[5]), exp(auto.arima.forecast$pred[6])))
-names(forecast)[1] <- "jobs"
+#adding sarima forecast points and lines
+points(n+1, sarima$pred[1])
+points(n+2, sarima$pred[2])
+points(n+3, sarima$pred[3])
+points(n+4, sarima$pred[4])
+points(n+5, sarima$pred[5])
+points(n+6, sarima$pred[6])
+lines(auto.arima.time, sarima.pred, type="l", col = "blue")
+
+#actual change in jobs
+
